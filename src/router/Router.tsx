d55f8ab263be0c2bc1767e5d90fe1eaa130d31bf
yilm01 <philosophia268@gmail.com>
2022-01-22 22:14:17 +0900

    コンフリクト解消しました。

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,21 +1,15 @@
 import { VFC } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../components/pages";
-<<<<<<< HEAD
 import CalendarDate from "../components/pages/calendar";
-=======
 import { Video } from "../components/pages/video";
->>>>>>> main
 
 export const Router: VFC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
-<<<<<<< HEAD
       <Route path="/calendar" element={<CalendarDate />}></Route>
-=======
       <Route path="/video" element={<Video />}></Route>
->>>>>>> main
     </Routes>
   );
 };
