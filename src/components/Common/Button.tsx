e3f4ee85155745e4a773c686dec5130d0ff7f031
aaaaua27 <nkgwyn72@gmail.com>
2022-01-22 18:28:29 +0900

    css移動

--- src/components/Common/Button.tsx
+++ src/components/Common/Button.tsx
@@ -1,9 +1,10 @@
 import { FC } from "react";
+import style from "./button.module.scss";
 
 type ButtonProps = {
   title: string;
   onClick: () => void;
 };
 export const Button: FC<ButtonProps> = (props) => {
-  return <button className="base-button" onClick={props.onClick}>{props.title}</button>;
+  return <button className={style.button} onClick={props.onClick}>{props.title}</button>;
 };
