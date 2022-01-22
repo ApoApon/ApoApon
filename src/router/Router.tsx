f1f74eb2edbf73d86fff19b30b18de938f1749d6
yilm01 <philosophia268@gmail.com>
2022-01-22 20:55:18 +0900

    初投稿でgooglecalendarだけ実装できてないや

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,11 +1,13 @@
 import { VFC } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../components/pages";
+import CalendarDate from "../components/pages/calendar";
 
 export const Router:VFC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/calendar" element={<CalendarDate />}></Route>
     </Routes>
   );
 };
