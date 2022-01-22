01cfcc7850035d7d0d729da046a9a7b62547cf0c
aaaaua27 <nkgwyn72@gmail.com>
2022-01-22 13:26:44 +0900

    レイアウト、ルーター追加

---
+++ src/components/pages/index.tsx
@@ -0,0 +1,10 @@
+import { VFC } from "react";
+import { Layout } from "../Layout/Layout";
+
+export const Home:VFC = () => {
+  return (
+    <Layout>
+      <div>Home</div>
+    </Layout>
+  );
+};
