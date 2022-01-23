b62cd1a6cdfb6c3f50152254b3173f4b9db2dc2e
TetsuOtter <31824852+TetsuOtter@users.noreply.github.com>
2022-01-23 14:34:03 +0900

    ログイン関係の処理を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,54 @@
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./router/Router";
+import { createContext, useState, useContext, useEffect } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
 import "./styles/App.scss";
+import { DBCtrler } from "./firebase/DBCtrler";
+import { FirebaseCredential } from "./firebase/FirebaseCredential";
+
+interface IAuthContext {
+  credential: FirebaseCredential;
+  dbCtrler: DBCtrler;
+  user: User | null;
+}
+
+const credential = new FirebaseCredential();
+const dbCtrler = new DBCtrler(credential.firestore);
+
+const AuthContext = createContext<IAuthContext>({
+  credential: credential,
+  dbCtrler: dbCtrler,
+  user: null,
+});
+
+export function useAuthContext(): IAuthContext {
+  return useContext(AuthContext);
+}
 
 function App() {
+  const [user_id, setUserID] = useState<User | null>(null);
+
+  const value: IAuthContext = {
+    credential: credential,
+    dbCtrler: dbCtrler,
+    user: user_id,
+  };
+
+  useEffect(
+    () =>
+      onAuthStateChanged(credential.auth, (user) => {
+        dbCtrler.user_id = user?.uid ?? "";
+        setUserID(user);
+      }),
+    []
+  );
+
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <AuthContext.Provider value={value}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
